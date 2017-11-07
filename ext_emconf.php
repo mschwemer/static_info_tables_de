e87012e200f4d1d23c57fd7f61b823f1d902480d
Marcus Schwemer <marcus.schwemer@in2code.de>
2017-11-07 11:26:17 +0100

    Inital commit based on TER version

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_de".
+ *
+ * Auto generated | Identifier: beda1d98f83881b94dae1edc8607456c
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Static Info Tables (de)',
+	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'version' => '6.4.1',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'author' => 'Daniel Minder',
+	'author_email' => 'typo3@minder.de',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-8.7.99',
+			'static_info_tables' => '6.3.1-6.99.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'clearcacheonload' => false,
+	'_md5_values_when_last_written' => 'a:57:{s:8:"Classes/";s:4:"d41d";s:15:"Classes/Domain/";s:4:"d41d";s:21:"Classes/Domain/Model/";s:4:"d41d";s:33:"Classes/Domain/Model/Language.php";s:4:"d6a6";s:36:"Classes/Domain/Model/CountryZone.php";s:4:"2e16";s:34:"Classes/Domain/Model/Territory.php";s:4:"94ac";s:32:"Classes/Domain/Model/Country.php";s:4:"3512";s:33:"Classes/Domain/Model/Currency.php";s:4:"b864";s:20:"class.ext_update.php";s:4:"2a45";s:13:"composer.json";s:4:"3f94";s:14:"Configuration/";s:4:"d41d";s:18:"Configuration/TCA/";s:4:"d41d";s:28:"Configuration/TCA/Overrides/";s:4:"d41d";s:50:"Configuration/TCA/Overrides/static_territories.php";s:4:"1062";s:48:"Configuration/TCA/Overrides/static_countries.php";s:4:"a115";s:48:"Configuration/TCA/Overrides/static_languages.php";s:4:"53fd";s:52:"Configuration/TCA/Overrides/static_country_zones.php";s:4:"a76d";s:49:"Configuration/TCA/Overrides/static_currencies.php";s:4:"d2f8";s:35:"Configuration/DomainModelExtension/";s:4:"d41d";s:55:"Configuration/DomainModelExtension/StaticInfoTables.txt";s:4:"b17a";s:25:"Configuration/TypoScript/";s:4:"d41d";s:33:"Configuration/TypoScript/Extbase/";s:4:"d41d";s:42:"Configuration/TypoScript/Extbase/setup.txt";s:4:"7912";s:14:"Documentation/";s:4:"d41d";s:26:"Documentation/Settings.yml";s:4:"02a1";s:27:"Documentation/Installation/";s:4:"d41d";s:36:"Documentation/Installation/Index.rst";s:4:"fb44";s:21:"Documentation/Images/";s:4:"d41d";s:35:"Documentation/Images/RecordView.png";s:4:"a005";s:41:"Documentation/Images/ExtensionManager.png";s:4:"219c";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:27:"Documentation/Introduction/";s:4:"d41d";s:36:"Documentation/Introduction/Index.rst";s:4:"a5a0";s:23:"Documentation/Index.rst";s:4:"f631";s:24:"Documentation/Changelog/";s:4:"d41d";s:33:"Documentation/Changelog/Index.rst";s:4:"a151";s:12:"ext_icon.png";s:4:"eda3";s:17:"ext_localconf.php";s:4:"3c90";s:14:"ext_tables.sql";s:4:"152a";s:25:"ext_tables_static+adt.sql";s:4:"842c";s:10:"Resources/";s:4:"d41d";s:17:"Resources/Public/";s:4:"d41d";s:23:"Resources/Public/Icons/";s:4:"d41d";s:36:"Resources/Public/Icons/Extension.svg";s:4:"828b";s:18:"Resources/Private/";s:4:"d41d";s:27:"Resources/Private/Language/";s:4:"d41d";s:46:"Resources/Private/Language/de.locallang_db.xlf";s:4:"7759";s:46:"Resources/Private/Language/it.locallang_db.xlf";s:4:"5e73";s:46:"Resources/Private/Language/pl.locallang_db.xlf";s:4:"fd56";s:46:"Resources/Private/Language/es.locallang_db.xlf";s:4:"725c";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"f294";s:46:"Resources/Private/Language/ro.locallang_db.xlf";s:4:"cfb5";s:46:"Resources/Private/Language/fr.locallang_db.xlf";s:4:"4f22";s:46:"Resources/Private/Language/ru.locallang_db.xlf";s:4:"c662";s:46:"Resources/Private/Language/fi.locallang_db.xlf";s:4:"bade";s:46:"Resources/Private/Language/br.locallang_db.xlf";s:4:"ec06";s:27:"Resources/Private/.htaccess";s:4:"5cc8";}',
+);
+
+?>
